5274f58e0db7462dacf4e4238f8526ecebf87bb7
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-08-09 07:32:40 -0400

    added button/destination effect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+    // sign up buttons
+let buttonA = document.querySelector(".content-pick .btn")
+let buttonB = document.querySelector(".content-pick .btn:nth-child(2)")
+let buttonC = document.querySelector(".content-pick .btn:nth-child(3)")
+
+let destA = document.querySelector(".content-pick .destination")
+let destB = document.querySelector(".content-pick .destination:nth-child(2)")
+let destC = document.querySelector(".content-pick .destination:nth-child(3)")
+
+function pushButton() {
+    destA.style.backgroundColor = "lightgrey"
+    destA.style.borderRadius = "20px"
+}
+function whiteBack(event) {
+    parent.style.backgroundColor = "white"
+}
+
+buttonA.addEventListener('mouseover', pushButton)
+buttonA.addEventListener('mouseout', whiteBack)
+
+
