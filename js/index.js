13b4da7684b6e9cc1028eaa3bf4a52bc6abe476b
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-08-10 13:41:46 -0400

    added wheel eventListener to images

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 // Your code goes here
     
 
-// sign up buttons
+// Elements
 let nav = document.getElementsByClassName("nav-link")
 let buttons = document.getElementsByClassName("btn")
+// let images = document.getElementsByTagName("img")
+let images = document.getElementsByClassName("img-fluid rounded");
 
-// functions
+const buttonArr = [...buttons]; // create array from HTMLCollection (or NodeList)
+const navArr = [...nav]; // create array from navigation
+const imagesArr = [...images]; // create array from images
 
+// Functions
 function pushButton() {
     this.parentElement.style.backgroundColor = "lightgrey"
     this.parentElement.style.borderRadius = "20px"  
@@ -16,25 +21,21 @@ function whiteBack() {
     this.parentElement.style.backgroundColor = "white" // change parent element
 }
 
-function keyPress(e) {
-    if (e.key === 'escape') {
-        alert('dont press the escape key')
-    }
 
-}
+// function contextMenu(e) {
+//     alert('right clicked!')
+// }
 
 
-// event listeners
-const buttonArr = [...buttons]; // create array from HTMLCollection (or NodeList)
-const navArr = [...nav]; // create array from navigation
+///////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-
-navArr.forEach(function(e, index){ // looping over each menu item in nav
-    e.addEventListener('click', () => {
+// Event listeners
+navArr.forEach(function(e){ // looping over each menu item in nav
+    e.addEventListener('dblclick', () => {
     e.style.backgroundColor = "red";
     })
+    // e.addEventListener('click', whiteBack);
 })
-
 
 buttonArr.forEach(function(e) {
     e.addEventListener('mouseover', pushButton) // looping over each element in array and adding event listener to it
@@ -42,7 +43,23 @@ buttonArr.forEach(function(e) {
 })
 
 
-document.addEventListener('keypress', keyPress)
 
+imagesArr.forEach((e) => {
+    e.addEventListener('wheel', () => {
+        if (e.getAttribute("src") === "img/fun.jpg") {
+        e.setAttribute("src", "img/adventure.jpg")
+        }
+        else {
+            e.setAttribute("src", "img/fun.jpg") 
+        }
+    })
+})
+
+
+// document.addEventListener('contextmenu', contextMenu)
+
+// console.log(imageArr[1])
+
+// console.log(imagesArr);
 
 
